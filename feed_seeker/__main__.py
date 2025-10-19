509dec14bfdd9445e7d231cdcdd34fdd4cc0d8b7
Thomas Vandal <thomas.vandal@umontreal.ca>
2025-10-19 16:21:34 -0400

    Handle multiple feeds in CLI

--- feed_seeker/__main__.py
+++ feed_seeker/__main__.py
@@ -1,6 +1,9 @@
 import sys
 from argparse import ArgumentParser
-from feed_seeker import find_feed_url, generate_feed_urls, find_feedly_feeds
+
+from feed_seeker import find_feed_url, find_feedly_feeds, generate_feed_urls
+
+# TODO: Used for tests, remove
 # https://ici.radio-canada.ca/rss
 
 
@@ -26,19 +29,46 @@ def main():
     psr.add_argument(
         "--max-links", help="Maximum links to check as feeds.", type=int, default=None
     )
-    psr.add_argument("--all", help="Find all feeds under the provided URL", action="store_true")
-    psr.add_argument("--feedly", help="Find all feeds under the provided URL using Feedly, takes precedence over '--all'", action="store_true")
+    psr.add_argument(
+        "--all", help="Find all feeds under the provided URL", action="store_true"
+    )
+    psr.add_argument(
+        "--feedly",
+        help="Find all feeds under the provided URL using Feedly, takes precedence over '--all'",
+        action="store_true",
+    )
     args = psr.parse_args()
 
+    defaults = {action.dest: action.default for action in psr._actions}
 
     feed_urls = []
-    # TODO: Handle args that are not used by feedly
     if args.feedly:
+        feedly_ignored_args = ["html", "spider", "max-time", "all"]
+        for arg_name in feedly_ignored_args:
+            py_arg_name = arg_name.replace("-", "_")
+            default_val = defaults[py_arg_name]
+            current_val = getattr(args, py_arg_name)
+            if default_val != current_val:
+                print(f"--{arg_name} is ignored when --feedly is used", file=sys.stderr)
         feed_urls += list(find_feedly_feeds(args.url, max_links=args.max_links))
     elif args.all:
-        feed_urls += list(generate_feed_urls(args.url, htø=args.html, spider=args.spider, max_time=args.max_time, max_links=args.max_links))
+        feed_urls += list(
+            generate_feed_urls(
+                args.url,
+                html=args.html,
+                spider=args.spider,
+                max_time=args.max_time,
+                max_links=args.max_links,
+            )
+        )
     else:
-        feed_url = find_feed_url(args.url, html=args.html, spider=args.spider, max_time=args.max_time, max_links=args.max_links)
+        feed_url = find_feed_url(
+            args.url,
+            html=args.html,
+            spider=args.spider,
+            max_time=args.max_time,
+            max_links=args.max_links,
+        )
         if feed_url is not None:
             feed_urls.append(feed_url)
 
